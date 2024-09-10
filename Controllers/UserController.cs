d70effb51638624852251f7593f5770c81131bb3
WeylinV <st10390916@vcconnect.edu.za>
2024-09-10 11:53:59 +0200

    Added user controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10390916_PROG_POE.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+    }
+}
