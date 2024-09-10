255a8d5f6dcb5ba3d1e8d2a2f6d6d1b5f3609432
WeylinV <st10390916@vcconnect.edu.za>
2024-09-10 11:53:27 +0200

    Added claims controller

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10390916_PROG_POE.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        public IActionResult ReviewClaims()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+    }
+}
