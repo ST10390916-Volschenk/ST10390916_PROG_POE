1e8c1b21ed0590818587e5d1d2b7aa3f5bbe8158
WeylinV <st10390916@vcconnect.edu.za>
2024-10-17 16:45:31 +0200

    Added claim functionality

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -1,16 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ST10390916_PROG_POE.Models;
 
 namespace ST10390916_PROG_POE.Controllers
 {
     public class ClaimsController : Controller
     {
+        [HttpGet]
         public IActionResult ReviewClaims()
         {
+            Claim claim = new Claim();
+            User user = new User();
+            List<Claim> claims = claim.GetPendingClaims();
+            ViewData["Claims"] = claims;
+            ViewData["User"] = user;
             return View();
         }
 
         public IActionResult SubmitClaim()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateClaimStatus(int claimID, ClaimStatus status)
+        {
+            Claim claim = new Claim();
+            claim.UpdateClaim(claimID, status);
+            return RedirectToAction("ReviewClaims", "Claims");
+        }
+
+        [HttpPost]
+        public IActionResult SubmitClaim(Claim claim)
+        {
+            claim.UserID = HttpContext.Session.GetInt32("UserID").Value;
+            claim.Status = ClaimStatus.Pending;
+            claim.ClaimDate = DateOnly.FromDateTime(DateTime.Now);
+            claim.SubmitClaim(claim);
+
+            return RedirectToAction("YourClaims", "Claims");
+        }
+
+        [HttpGet]
+        public IActionResult YourClaims()
+        {
+            Claim claim = new Claim();
+            User user = new User();
+            user = user.GetUser(HttpContext.Session.GetInt32("UserID").Value);
+            List<Claim> claims = claim.GetClaimsByUserID(user.UserID);
+            ViewData["Claims"] = claims;
             return View();
         }
 
--- Models/Claim.cs
+++ Models/Claim.cs
@@ -1,6 +1,56 @@
-﻿namespace ST10390916_PROG_POE.Models
+﻿using ST10390916_PROG_POE.Data;
+
+namespace ST10390916_PROG_POE.Models
 {
     public class Claim
     {
+        public int ClaimID { get; set; }
+        public int UserID { get; set; }
+        public string ClaimTitle { get; set; }
+        public int HoursWorked { get; set; }
+        public double RatePerHour { get; set; }
+        public ClaimStatus Status { get; set; }
+        public DateOnly ClaimDate { get; set; }
+
+        public string SubmitClaim(Claim claim)
+        {
+            AppDbContext context = new AppDbContext();
+            context.claims.Add(claim);
+            context.SaveChanges();
+
+            return "Claim submitted.";
+        }
+
+        public List<Claim> GetClaimsByUserID(int userID)
+        {
+            AppDbContext context = new AppDbContext();
+            List<Claim> claims = context.claims.Where(c => c.UserID == userID).ToList();
+            return claims;
+        }
+
+        public List<Claim> GetPendingClaims()
+        {
+            AppDbContext context = new AppDbContext();
+            List<Claim> claims = context.claims.Where(c => c.Status == ClaimStatus.Pending).ToList();
+            return claims;
+        }
+
+        public void UpdateClaim(int claimID, ClaimStatus status)
+        {
+            AppDbContext context = new AppDbContext();
+            context.claims.Find(claimID).Status = status;
+            context.SaveChanges();
+        }
+
     }
+
+    public enum ClaimStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+
+
+
 }
