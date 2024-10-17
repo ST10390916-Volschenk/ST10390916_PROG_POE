b249b5c1327adaee0f70c196421baaed4e21d98a
WeylinV <st10390916@vcconnect.edu.za>
2024-10-17 16:43:25 +0200

    Added Database functionality.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ST10390916_PROG_POE.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//using context accessor to make use of sessions. set session limit to 2 hours
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(Options =>
+{
+    Options.IdleTimeout = TimeSpan.FromMinutes(120);
+});
+
+/*builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));*/
 
 var app = builder.Build();
 
@@ -19,6 +33,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+//added to make use of sessions to keep login data
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
