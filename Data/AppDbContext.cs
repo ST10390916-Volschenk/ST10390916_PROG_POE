5444b4c885e2dada8c40ddf1125ca2de5c168cd1
WeylinV <st10390916@vcconnect.edu.za>
2024-10-17 16:45:53 +0200

    Updated database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ST10390916_PROG_POE.Models;
+
+
+namespace ST10390916_PROG_POE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        /*public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }*/
+        
+        public AppDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Database=ClaimsDB;Trusted_Connection=True");
+        }
+
+        
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Claim> claims { get; set; }
+
+    }
+}
